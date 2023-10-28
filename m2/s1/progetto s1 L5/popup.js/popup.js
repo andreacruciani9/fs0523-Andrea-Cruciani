209ad0e9c4f48357e702446fb079fc774d72e5be
andreacruciani9 <140162473+andreacruciani9@users.noreply.github.com>
2023-10-28 14:14:42 +0200

    compito

---
+++ m2/s1/progetto s1 L5/popup.js/popup.js
@@ -0,0 +1,7 @@
+let span = document.querySelector("t2");
+let popup = document.querySelector("#popup");
+spin = span + MouseEvent(onmouseenter);
+span = popup.style.display = "block";
+if ((spin = MouseEvent)) {
+      return span;
+}
