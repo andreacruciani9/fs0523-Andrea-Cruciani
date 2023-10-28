074fdd4f98626b971e3d730e63e0ac66b0931d38
andreacruciani9 <140162473+andreacruciani9@users.noreply.github.com>
2023-10-28 14:19:41 +0200

    compito

--- m2/s1/progetto s1 L5/popup.js/popup.js
+++ m2/s1/progetto s1 L5/popup.js/popup.js
@@ -1,7 +1,5 @@
 let span = document.querySelector("t2");
 let popup = document.querySelector("#popup");
-spin = span + MouseEvent(onmouseenter);
-span = popup.style.display = "block";
-if ((spin = MouseEvent)) {
-      return span;
-}
+span.addEventListener("MouseEvent(onmouseenter)", () => {
+      popup.style.display = block;
+});
