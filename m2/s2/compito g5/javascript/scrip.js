f8c73867a35a1aa1b29b1d58433444ba7e8ce80c
andreacruciani9 <140162473+andreacruciani9@users.noreply.github.com>
2023-11-06 04:43:40 +0100

    s6-g5-compito

---
+++ m2/s2/compito g5/javascript/scrip.js
@@ -0,0 +1,6 @@
+let button = document.querySelector("dropbtn");
+
+button.addEventListener("click", () => {
+      let link = document.querySelectorAll("a");
+      link.style.display = "block";
+});
